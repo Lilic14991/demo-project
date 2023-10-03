1897ac8fcc9560f4a56ceeb3247410c53290adef
Lazar Ilic <lilic14991@gmail.com>
2023-10-03 16:41:47 +0200

    created CRUD operations on user entity

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Body, Post } from '@nestjs/common';
+import { Controller, Body, Post, Get, Put, Param, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UUID } from 'crypto';
 
-@Controller('user')
+@Controller('api/user')
 export class UserController {
     constructor(
         private readonly userService: UserService) {}
@@ -11,5 +12,21 @@ export class UserController {
     crete(@Body() dto: CreateUserDto) {
         
         return this.userService.create(dto);
+    }
+
+    @Get()
+    findAll() {
+        
+        return this.userService.findAll();
+    }
+
+    @Put(':id')
+    update(@Param('id') id: UUID, @Body() dto: CreateUserDto) {
+        return this.userService.update(id, dto);
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id: UUID) {
+        return this.userService.delete(id);
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UUID } from 'crypto';
 
+@Injectable()
 export class UserService {
     constructor(
         @InjectRepository(User)
@@ -13,5 +15,24 @@ export class UserService {
         const user = this.userRepository.create(dto)
 
         return await this.userRepository.save(user);
+    }
+
+    async findAll() {
+        return await this.userRepository.find();
+    }
+
+    async update(id: UUID, dto: CreateUserDto) {
+        const user = await this.userRepository.findOne({where: {id}});
+        
+        if(!user) {
+            throw new NotFoundException(`User with id: ${id} not found`)
+        }
+
+        Object.assign(user, dto)
+        return await this.userRepository.save(user);
+    }
+
+    async delete(id: UUID) {
+        return await this.userRepository.delete(id);
     }
 }
